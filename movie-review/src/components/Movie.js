53115bd4ac32f9ca3bc0bac464d114fcc9ab8c55
Sarahellora <sarahellora1996@gmail.com>
2023-02-20 14:20:04 -0700

    added layout

---
+++ movie-review/src/components/Movie.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Movie = (props) => {
+
+    const movies=[{
+        title:"bob the builder",
+        review:"this movie was teriblle",
+        rating: 4
+    }]    
+return (
+    <div>
+        testing
+
+    </div>)
+}
+export default Movie
---
+++ movie-review/src/components/MovieList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Movie from './Movie'
+
+const MovieList = (props) => {
+
+  
+return (
+    <div>
+        <Movie/>
+
+    </div>)
+}
+export default MovieList
